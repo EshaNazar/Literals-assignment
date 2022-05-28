35d3b14b4ee1f47599ec383e614752e975f25be3
Eshanazar19 <106012169+EshaNazar@users.noreply.github.com>
2022-05-28 23:51:16 +0500

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,13 @@
+public class Dowhile
+{
+     public static void main(String args[])
+    {
+         int j=1;
+         do
+               {
+                  System.out.println("HI");
+                  j++;
+                }
+           while(j<=5);
+      }
+}
