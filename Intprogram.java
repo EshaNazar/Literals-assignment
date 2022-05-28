60c2086ce16e0cac870647613de8d9f6d7048386
Eshanazar19 <106012169+EshaNazar@users.noreply.github.com>
2022-05-28 23:12:56 +0500

    Add files via upload

---
+++ Intprogram.java
@@ -0,0 +1,12 @@
+public class Intprogram
+{
+     public static void main(String args[])
+    {
+        int i=1;
+       while(i<4)
+               {
+                  System.out.println("HI");
+                  i++;
+                }
+      }
+}
