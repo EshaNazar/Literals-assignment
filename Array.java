9b3b3bacebafeb386ca02530996855f3e25e325d
Eshanazar19 <106012169+EshaNazar@users.noreply.github.com>
2022-05-28 23:44:22 +0500

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array
+{
+     public static void main(String args[])
+    {
+        int[] nums={20,25,13,14,16,29};
+        for(int i=0; i<6; i++)
+               {
+                  System.out.println("nums[i]");
+                }
+      }
+}
