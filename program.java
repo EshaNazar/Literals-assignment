6f427a8de81654fc03f366bd83bf06dfcca9dd25
Eshanazar19 <106012169+EshaNazar@users.noreply.github.com>
2022-06-09 17:16:13 +0500

    Add files via upload

---
+++ program.java
@@ -0,0 +1,18 @@
+class Addition 
+{
+  int x,y;
+     
+   Addition()
+  { 
+       x=10;
+       y=20;
+  }
+}
+public class program
+ {
+    public static void main(String args[])
+    {
+         Addition P=new Addition();
+         System.out.println(P.x+P.y);
+    }
+ }
