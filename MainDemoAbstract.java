fb8809b71785b37daa17d1a177fdf4e53dad924e
Eshanazar19 <106012169+EshaNazar@users.noreply.github.com>
2022-06-30 21:00:32 +0500

    Add files via upload

---
+++ MainDemoAbstract.java
@@ -0,0 +1,48 @@
+abstract class Figure
+{
+  double dim1;
+  double dim2;
+  Figure(double x, double y)
+   {
+     dim1=x;
+     dim2=y;
+   }
+  abstract double area();
+}
+class Triangle extends Figure
+{
+   Triangle( double x, double y)
+   {
+     super(x,y);
+   }
+  double area()
+   {
+    return 1.0/2.0*dim1 *dim2;
+   }
+}
+class Square extends Figure
+{
+  Square(double x, double y)
+  {
+    super(x,y);
+  }
+  double area()
+  {
+   return dim1*dim2;
+  }
+}
+class MainDemoAbstract
+{
+  public static void main(String arg[])
+  {
+    Figure f;
+    Triangle t= new Triangle(2.3,4.5);
+    Square   s= new Square(10,20);
+            f=t;
+    double at= f.area();
+    System.out.println("at:" + at);
+    f= new Square(200,3);
+    double as=f.area();
+    System.out.println("as:"+ as);
+   }
+}
