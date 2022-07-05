07554f4cb8d7438a4738a323a3ade4d52e86a303
Eshanazar19 <106012169+EshaNazar@users.noreply.github.com>
2022-07-05 20:37:34 +0500

    Add files via upload

---
+++ math.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+public class math 
+{
+    public static void main (String [] args)
+   { 
+     int x=4;
+     int y=6;
+     System.out.println(Math.min(x,y));
+     System.out.println(Math.max(x,y));
+     System.out.println(Math.pow(x,y));
+     System.out.println(Math.pow(y,x));
+    System.out.println(Math.sqrt(y));
+     System.out.println(Math.log(x));
+   }
+}
