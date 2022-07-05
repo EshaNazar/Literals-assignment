a0b172b3ec772e2578b8e4483d57fe79d6ef9480
Eshanazar19 <106012169+EshaNazar@users.noreply.github.com>
2022-07-05 20:38:15 +0500

    Add files via upload

---
+++ string.java
@@ -0,0 +1,13 @@
+import java.lang.String;
+public class string
+{
+    public static void main (String [] args)
+   {  
+   String i="";
+   char arrch[]={'h','e','l','l','o'};
+   String obj= new String (arrch);
+   System.out.println(obj);
+   System.out.println(i.isEmpty());
+   System.out.println(obj.isEmpty());
+   }
+}
