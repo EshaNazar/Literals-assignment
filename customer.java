6b5bfc41d096b9d7534de801d97872db3b9c72e3
Eshanazar19 <106012169+EshaNazar@users.noreply.github.com>
2022-08-04 15:47:20 +0500

    Add files via upload

---
+++ customer.java
@@ -0,0 +1,15 @@
+package koib; 
+ public class customer
+{ 
+ public  String name;
+ public  int age;
+public  void setvalue( String a, int b)
+  {
+  name=a;
+  age=b;
+}
+public void show()
+{
+ System.out.println("Name is: "+name+ "\nAge is :"+age);
+}
+}
