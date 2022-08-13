8e8315789e0ee5f3f7cc28964aba9ea84f0af4a3
Eshanazar19 <106012169+EshaNazar@users.noreply.github.com>
2022-08-14 00:51:23 +0500

    Add files via upload

---
+++ MainDemoGUIArray.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
+class MainDemoGUIArray
+{
+	public static void main(String[] arg)
+	{
+		JFrame frm=new JFrame();
+		Container con=frm.getContentPane();//Get Workable area
+		FlowLayout flw= new FlowLayout();//create layout object
+		con.setLayout(flw);//set the layout of workable area
+		//create GUI controls
+		JButton[] btn= new JButton[20];
+		for(int i=0;i<btn.length;i++)
+		{
+			btn[i]=new JButton((i+1)+"");
+			con.add(btn[i]);
+		}
+		frm.setSize(200,400);
+		frm.setVisible(true);
+		frm.setDefaultCloseOperation(3);		
+	}
+
+}
